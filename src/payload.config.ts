711c110c16fb9a22e3f97703dcb391afe82a1d7b
Debby Ji <jasmine820921@gmail.com>
2026-03-18 23:38:52 +0800

    feat: add region for s3adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -39,12 +39,15 @@ export default buildConfig({
       collections: {
         media: true,
       },
-      bucket: process.env.S3_BUCKET,
+      bucket: process.env.S3_BUCKET || '',
       config: {
+        endpoint: process.env.S3_ENDPOINT,
         credentials: {
           accessKeyId: process.env.S3_ACCESS_KEY_ID,
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
         },
+        region: 'auto',
+        forcePathStyle: true,
       },
     }),
   ],
